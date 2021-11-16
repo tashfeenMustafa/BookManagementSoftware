11207b49f5811541706781913fd4b60972d49473
Tashfeen Mustafa Choudhury <tashfeenc1234@gmail.com>
2021-11-16 12:13:51 +0600

    Clicking on books from the list will now take the user to a separate page for the book

--- client/src/AllBooks/BooksListItem.js
+++ client/src/AllBooks/BooksListItem.js
@@ -5,7 +5,10 @@ import Checkbox from '@mui/material/Checkbox';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Grid from '@mui/material/Grid';
+import { Link } from 'react-router-dom';
+
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import './BooksListItem.css'
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -15,63 +18,75 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           margin: 'auto',
+          color: '#000'
         },
       },
     },
+    MuiTypography: {
+        styleOverrides: {
+            root: {
+                color: '#000'
+            }
+        }
+    }
   },
 });
 
 export default function BooksListItem(props) {
     return (
-        <Box 
-        key={props.index} 
-        sx={{ 
-            margin: '1% 0 1% 0',
-            p: '12px 8px 12px 8px', 
-            border: '1px solid grey' 
-        }}>
-            <Grid 
-                container 
-                spacing={2}>
+        <Link
+            className="bookItemClass" 
+            to={`/books/${props.book.bookTitle}`}>
+            <Box 
+            key={props.index} 
+            sx={{ 
+                margin: '1% 0 1% 0',
+                p: '12px 8px 12px 8px', 
+                border: '1px solid grey' 
+            }}>
                 <Grid 
-                    item 
-                    xs={6} 
-                    sm={6} 
-                    md={6} 
-                    lg={6}>
-                    <Checkbox {...label} />
-                    <Typography component="span" sx={{ margin: 'auto' }}>
-                        {props.book.bookTitle }      
-                    </Typography>
-                    
-                </Grid>
-                <Grid 
-                    item 
-                    xs={6} 
-                    sm={6} 
-                    md={6} 
-                    lg={6}>
-                        <ThemeProvider theme={theme}>
-                            <div style={{
-                                margin: 'auto',
-                                float: 'right',
-                                width: '10%',
-                                paddingTop: '7px'
-                            }}>
-                                <span style={{
-                                    paddingRight: '15px',
+                    container 
+                    spacing={2}>
+                    <Grid 
+                        item 
+                        xs={6} 
+                        sm={6} 
+                        md={6} 
+                        lg={6}>
+                        <Checkbox {...label} />
+                        <Typography component="span" sx={{ margin: 'auto' }}>
+                            {props.book.bookTitle}      
+                        </Typography>
+                        
+                    </Grid>
+                    <Grid 
+                        item 
+                        xs={6} 
+                        sm={6} 
+                        md={6} 
+                        lg={6}>
+                            <ThemeProvider theme={theme}>
+                                <div style={{
+                                    margin: 'auto',
+                                    float: 'right',
+                                    width: '10%',
+                                    paddingTop: '7px'
                                 }}>
-                                    <EditIcon />
-                                </span>
-                                <span style={{
-                                    paddingRight: '10px'
-                                }}>
-                                    <DeleteIcon />
-                                </span>
-                            </div>
-                        </ThemeProvider>
+                                    <span style={{
+                                        paddingRight: '15px',
+                                    }}>
+                                        <EditIcon />
+                                    </span>
+                                    <span style={{
+                                        paddingRight: '10px'
+                                    }}>
+                                        <DeleteIcon />
+                                    </span>
+                                </div>
+                            </ThemeProvider>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </Link>
     )
 }
